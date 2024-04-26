c3d045c9530785cf18b4861fa5cd2c6efbd4d1d9
Nick Kaidash <kaidash.nikita93@gmail.com>
2024-04-26 14:05:52 +0300

    hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import { Editor, EditorContext } from '../lib/main';
 import SmallEditor from './components/SmallEditor.tsx';
-import '../lib/index.css';
 
-// import { Editor } from '../dist/main';
+import '../lib/index.css';
 
 function App() {
   const [pasteText, setPasteText] = useState<string>('')
   return (
     <div className="bg-gray-50 dark:bg-slate-800 transition-colors duration-500">
       <EditorContext>
-        <Editor placeholderText='...' pasteText={pasteText} />
+        <Editor placeholderText='Paste selected phrase...' pasteText={pasteText} />
       </EditorContext>
       <SmallEditor onSave={(text: string) =>  setPasteText(text)} />
     </div>
