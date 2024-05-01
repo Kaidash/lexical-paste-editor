e51729730751d833d69aab1901e0e23f225abef1
Nick Kaidash <kaidash.nikita93@gmail.com>
2024-05-01 19:33:06 +0300

    hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Editor, EditorContext } from '../lib/main.tsx';
 import SmallEditor from './components/SmallEditor.tsx';
 
